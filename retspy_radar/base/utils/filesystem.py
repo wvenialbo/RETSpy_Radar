ed7cce7159166b1c450854406b54ba5de1e62f5c
wvenialbo <wvenialbo@gmail.com>
2024-09-04 05:10:06 -0400

    refactor(base): improve filesystem utilities

--- retspy_radar/base/utils/filesystem.py
+++ retspy_radar/base/utils/filesystem.py
@@ -1,76 +1,113 @@
+"""
+Módulo con utilidades para trabajar con archivos.
+
+Las funciones de este módulo permiten verificar si un archivo es
+legible, creable o escribible. Las funciones de verificación de
+legibilidad y creabilidad son `ensure_readable_file` y
+`ensure_creatable_or_writeable_file`, respectivamente.
+
+La función `ensure_readable_file` verifica si un archivo es legible. Si
+el archivo no existe o no es legible, o la ruta no apunta a un archivo
+se lanza una excepción. La función `ensure_creatable_or_writeable_file`
+verifica si un archivo es creable o escribible. Si el archivo no es
+creable o escribible, o la ruta no apunta a un archivo se lanza una
+excepción.
+
+Las clases de este módulo son:
+
+    - `filesystem`: Clase con utilidades para trabajar con archivos.
+
+Las funciones de este módulo son:
+
+    - `ensure_readable_file`: Verifica si un archivo es legible.
+    - `ensure_creatable_or_writeable_file`: Verifica si un archivo es
+        creable o escribible.
+
+Las constantes de este módulo son:
+
+    - `FILE_NOT_FOUND_ERROR`: Mensaje de error para archivo no
+        encontrado.
+    - `IS_A_DIRECTORY_ERROR`: Mensaje de error para objeto es un
+        directorio.
+    - `NOT_A_FILE_ERROR`: Mensaje de error para objeto no es un archivo.
+    - `READ_PERMISSION_ERROR`: Mensaje de error para archivo no es
+        legible.
+    - `WRITE_PERMISSION_ERROR`: Mensaje de error para archivo no es
+        escribible.
+"""
+
 import os
 
 from ..exceptions import NotAFileError
 
-FILE_NOT_FOUND_ERROR = "No se encontró la ruta especificada"
-NOT_A_FILE_ERROR = "El objeto no es un archivo"
-READ_PERMISSION_ERROR = "El archivo no es legible"
-WRITE_PERMISSION_ERROR = "El archivo no es escribible"
-IS_A_DIRECTORY_ERROR = "El objeto es un directorio"
+# Mensajes de error
+
+FILE_NOT_FOUND_ERROR = "La ruta especificada no existe: '%s'"
+IS_A_DIRECTORY_ERROR = "El objeto es un directorio '%s'"
+NOT_A_FILE_ERROR = "El objeto no es un archivo: '%s'"
+READ_PERMISSION_ERROR = "El archivo no es legible: '%s'"
+WRITE_PERMISSION_ERROR = "El archivo no es escribible: '%s'"
 
 
-class filesystem:
+class filesystem:  # pylint: disable=invalid-name
+    """
+    Clase con utilidades para trabajar con archivos.
+    """
+
     @staticmethod
-    def ensure_readable_file(file_path: str) -> None:
+    def ensure_readable_file(
+        file_path: str, *, optional: bool = False
+    ) -> bool:
         """
         Asegura que un archivo sea legible.
 
-        Verifica que un archivo exista y sea legible. Si el archivo no
-        existe o no es legible, o la ruta no apunta a un archivo se
-        lanza una excepción.
+        Si `optional` es `False`, verifica que un archivo exista y sea
+        legible. Si el archivo no existe o no es legible, o la ruta no
+        apunta a un archivo se lanza una excepción. Si `optional` es
+        `True` y el archivo no existe, se retorna `False`; en caso
+        contrario, se retorna `True` si el archivo es legible.
+
+        Verifica
 
         Parameters
         ----------
         file_path : str
             La ruta del archivo a verificar.
+        optional : bool, optional
+            Si se debe lanzar una excepción si el archivo no existe, por
+            defecto es `False`.
+
+        Returns
+        -------
+        bool
+            `True` si el archivo existe y es legible, `False` si el
+            archivo no existe y `optional` es `True`.
 
         Raises
         ------
         FileNotFoundError
             Si el archivo no existe.
+        IsADirectoryError
+            Si la ruta apunta a un directorio.
         NotAFileError
             Si la ruta no apunta a un archivo.
         PermissionError
             Si el archivo no es legible.
         """
         if not os.path.exists(file_path):
-            raise FileNotFoundError(FILE_NOT_FOUND_ERROR)
+            if optional:
+                return False
+
+            raise FileNotFoundError(FILE_NOT_FOUND_ERROR % file_path)
+
+        if os.path.isdir(file_path):
+            raise IsADirectoryError(IS_A_DIRECTORY_ERROR % file_path)
 
         if not os.path.isfile(file_path):
-            raise NotAFileError(NOT_A_FILE_ERROR)
+            raise NotAFileError(NOT_A_FILE_ERROR % file_path)
 
         if not os.access(file_path, os.R_OK):
-            raise PermissionError(READ_PERMISSION_ERROR)
-
-    @staticmethod
-    def ensure_optional_file(file_path: str) -> bool:
-        """
-        Asegura que un archivo sea legible si existe.
-
-        Verifica que un archivo sea legible si existe. Si el archivo
-        existe y no es legible, o la ruta no apunta a un archivo, se
-        lanza una excepción.
-
-        Parameters
-        ----------
-        file_path : str
-            La ruta del archivo a verificar.
-
-        Raises
-        ------
-        NotAFileError
-            Si la ruta no apunta a un archivo.
-        PermissionError
-            Si el archivo no es legible.
-        """
-        if not os.path.exists(file_path):
-            return False
-
-        if not os.path.isfile(file_path):
-            raise NotAFileError(NOT_A_FILE_ERROR)
-
-        if not os.access(file_path, os.R_OK):
-            raise PermissionError(READ_PERMISSION_ERROR)
+            raise PermissionError(READ_PERMISSION_ERROR % file_path)
 
         return True
 
@@ -101,10 +138,10 @@ class filesystem:
             return
 
         if os.path.isdir(file_path):
-            raise IsADirectoryError(IS_A_DIRECTORY_ERROR)
+            raise IsADirectoryError(IS_A_DIRECTORY_ERROR % file_path)
 
         if not os.path.isfile(file_path):
-            raise NotAFileError(NOT_A_FILE_ERROR)
+            raise NotAFileError(NOT_A_FILE_ERROR % file_path)
 
         if not os.access(file_path, os.W_OK):
-            raise PermissionError(WRITE_PERMISSION_ERROR)
+            raise PermissionError(WRITE_PERMISSION_ERROR % file_path)
